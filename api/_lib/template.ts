03393fa40df036a2d1708030cbff405a51225c07
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-03-10 21:24:23 +0900

    remove background

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -47,8 +47,8 @@ function getCss(theme: string, fontSize: string) {
 
     body {
         background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
+        // background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+        // background-size: 100px 100px;
         height: 100vh;
         display: flex;
         text-align: center;
